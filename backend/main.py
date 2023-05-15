60a09a5b35bee816f18c91844e2589c16c60a2b1
pandyapankil <pandyapankil022@gmail.com>
2023-05-15 10:35:39 +0530

    TODOs added

--- backend/main.py
+++ backend/main.py
@@ -50,10 +50,11 @@ async def scrap(search_keywords, products_count):
 				print('Product Rating:', product_rating)
 
 			# Extract the product URL
-			product_url = result.find(
+			product_relative_url = result.find(
 				'a', {'class': 'a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal'}
 			)['href']
-			print('Product URL:', f"{base_url}{product_url}")
+			product_url = f"{base_url}{product_relative_url}"
+			print('Product URL:', product_url)
 
 			print('-' * 50)
 
@@ -82,6 +83,17 @@ async def get_products(search_keywords: str, products_count: int = 10):
 
 	return result
 
+
+# TODO: 1 separate endpoint which will take an email id from user if products_count is g.t. 20
+# that will be handled using celery with redis broker in background and then result will be sent over email
+# TODO: 1-1 print the response first over console
+# TODO: 1-2 send to the email provided
+# TODO: 2 start with the simplest DB to store the result against keyword; key-value store is a good choice
+# TODO: 3 serve DB separately
+# TODO: 4 containerize the app
+# TODO: 5 add jwt auth to restrict usage to authenticated users
+# TODO:
+# TODO: 6 add kubernetess if time permits
 
 if __name__ == "__main__":
 	uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
